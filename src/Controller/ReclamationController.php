ab625f4b83c4faea39ba9f50b330dbb9008ea288
sarra <sarramzali12@gmail.com>
2025-01-03 00:53:07 +0100

    reclamtion

---
+++ src/Controller/ReclamationController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Reclamation;
+use App\Form\ReclamationType;
+use App\Repository\ReclamationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/reclamation')]
+final class ReclamationController extends AbstractController
+{
+    #[Route('/', name: 'app_reclamation_index', methods: ['GET'])]
+    public function index(ReclamationRepository $reclamationRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $reclamations = $reclamationRepository->findAll();
+        return $this->render('reclamation/index.html.twig', [
+            'reclamations' => $reclamations,
+        ]);
+    }
+
+
+    #[Route('/new', name: 'app_reclamation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            $this->addFlash('error', 'Vous devez être connecté pour soumettre une réclamation.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($request->isMethod('POST')) {
+            $contenu = $request->request->get('contenu');
+
+            if (empty($contenu)) {
+                $this->addFlash('error', 'Le contenu de la réclamation est obligatoire.');
+                return $this->redirectToRoute('app_reclamation_new');
+            }
+
+            $reclamation = (new Reclamation())
+                ->setContenu($contenu)
+                ->setDate(new \DateTime())
+                ->setUser($user);
+
+            $entityManager->persist($reclamation);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Réclamation soumise avec succès.');
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('reclamation/new.html.twig');
+    }
+
+    #[Route('/{id}', name: 'app_reclamation_show', methods: ['GET'])]
+    public function show(Reclamation $reclamation): Response
+    {
+        return $this->render('reclamation/show.html.twig', [
+            'reclamation' => $reclamation,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_reclamation_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Reclamation $reclamation, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ReclamationType::class, $reclamation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_reclamation_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('reclamation/edit.html.twig', [
+            'reclamation' => $reclamation,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_reclamation_delete', methods: ['POST'])]
+    public function delete(Request $request, Reclamation $reclamation, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$reclamation->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($reclamation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_reclamation_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
